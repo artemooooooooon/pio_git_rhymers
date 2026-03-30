304a95716d1850181ba2294a20257f2d590d488d
Bartosz979 <bartoszwaszczuk979@gmail.com>
2026-03-30 15:05:06 +0200

    8.2 Dodanie brakujących stałej DEFAULT_RETURN_VALUE i zaimplementowanie nieużytych stałych w kodzie

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,6 +3,7 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
     //Dodanie keyword final do niemutowalnych atrybutów klasy
     public static final int TOTAL = -1;
+    public static final int DEFAULT_RETURN_VALUE = -1
     public static final int ARRAY_LENGTH = 12;
     private int[] numbers = new int[ARRAY_LENGTH];
 
@@ -18,18 +19,18 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == ARRAY_LENGTH - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return TOTAL;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return TOTAL;
+            return DEFAULT_RETURN_VALUE;
         return numbers[total--];
     }
 
