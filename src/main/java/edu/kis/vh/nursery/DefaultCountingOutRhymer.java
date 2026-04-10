a81b6f70247820c4d9a3fae12684b43027b63046
artemooooooooon <it.artemon@gmail.com>
2026-04-10 21:16:39 +0200

    drobna korekta

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
     //Dodanie keyword final do niemutowalnych atrybutów klasy
     public static final int TOTAL = -1;
-    public static final int DEFAULT_RETURN_VALUE = -1
+    public static final int DEFAULT_RETURN_VALUE = -1;
     public static final int ARRAY_LENGTH = 12;
     private int[] numbers = new int[ARRAY_LENGTH];
 
