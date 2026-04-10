0c4f1e32af2d901bc8b5fe8db10947563abbaf8d
artemooooooooon <it.artemon@gmail.com>
2026-04-10 21:53:42 +0200

    hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
     //Dodanie keyword final do niemutowalnych atrybutów klasy
-    public static final int TOTAL = -1;
-    public static final int DEFAULT_RETURN_VALUE = -1;
-    public static final int ARRAY_LENGTH = 12;
+    private static final int TOTAL = -1;
+    private static final int DEFAULT_RETURN_VALUE = -1;
+    private static final int ARRAY_LENGTH = 12;
     private int[] numbers = new int[ARRAY_LENGTH];
 
-    public int total = TOTAL;
+    private int total = TOTAL;
 
     public void countIn(int in) {
         if (!isFull())
